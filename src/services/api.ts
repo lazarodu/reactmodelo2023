33b045aa971c421e0813e395db54fffd04bafb80
lazarodu <lazarodu@gmail.com>
2023-11-17 07:16:39 -0300

    aula 17 11 2023

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333',
+  baseURL: 'https://apiadonis2023.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
